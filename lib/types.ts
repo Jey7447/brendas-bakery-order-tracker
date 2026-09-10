1ef5e435e3a8fa804c6c5f06163dd31e9ab5805c
Jey7447 <108633026+Jey7447@users.noreply.github.com>
2026-09-10 16:17:45 +0100

    Add order types

---
+++ lib/types.ts
@@ -0,0 +1,31 @@
+export type PaymentStatus = "PAID" | "UNPAID";
+export type OrderStatus =
+  | "PENDING"
+  | "CONFIRMED"
+  | "PREPARING"
+  | "READY"
+  | "OUT_FOR_DELIVERY"
+  | "DELIVERED"
+  | "CANCELLED";
+
+export type OrderItem = {
+  name: string;
+  quantity: number;
+  unitPrice: number;
+};
+
+export type Order = {
+  id: string;
+  createdAt: string;
+  customerName: string;
+  phone: string;
+  email: string;
+  items: OrderItem[];
+  deliveryDate: string;
+  deliveryTime: string;
+  address: string;
+  total: number;
+  paymentStatus: PaymentStatus;
+  orderStatus: OrderStatus;
+  notes?: string;
+};
